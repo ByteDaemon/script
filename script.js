7b99d1235ba4c6a7364de0b31135a7d52075de5e
Swarang Londhe <swaranglondhe49@gmail.com>
2024-10-21 17:41:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+for (let i = 0; i < 10; i++) {
+    let rows = document.querySelectorAll("table tr");
+
+    rows.forEach(row => {
+        let radios = row.querySelectorAll('input[type="radio"]');
+
+        if (radios.length >= 1) {
+            radios[0].checked = true;
+        }
+    });
+
+    document.getElementById('btnSaveFeedBack').click();
+}
+
+
